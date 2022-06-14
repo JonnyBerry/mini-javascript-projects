932813ca51b70953792881edcb68d33abf4d0e74
jonathanberry <jonathanberry45@gmail.com>
2022-06-15 00:18:52 +0100

    added color scheme for buutons

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -17,6 +17,11 @@ btns.forEach(function (btn) {
    else{
      count = 0;
    }
+   if(count > 0){
+     value.style.color = "green";
+   }
+   else if(count < 0)
+   value.style.color = "red"
    value.textContent = count;
  });
 });
