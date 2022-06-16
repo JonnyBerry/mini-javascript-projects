3bd6c669dbb986a3b6994fd32935ae0713260e94
jonathanberry <jonathanberry45@gmail.com>
2022-06-16 14:17:42 +0100

    set up showperson function in app.js

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -44,6 +44,7 @@ const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
 
+//click events
 const prevBtn = document.querySelector('.prev-btn')
 const nextBtn = document.querySelector('.next-btn')
 const randomBtn = document.querySelector('.random-btn')
@@ -53,5 +54,15 @@ let currentItem = 0;
 
 //load initial item
 window.addEventListener('DOMContentLoaded',function(){
-  console.log('shake and bake')
-})
+showPerson(currentItem);
+});
+
+// show person based on item
+
+function showPerson(person){
+const item =reviews[person]
+  img.src = item.img;
+  author.textContent =item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
